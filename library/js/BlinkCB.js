90c70e23a52eb101ba402b337bd0f217632bb2dd
shari <sharipkanov@gmail.com>
2016-03-17 17:13:34 +0600

    JS HTML CSS for mailer

--- library/js/BlinkCB.js
+++ library/js/BlinkCB.js
@@ -120,6 +120,7 @@ var loadTmpJS = function() {
 
             data[inputName] = inputValue;
         }
+        data = JSON.stringify.(data);
         var xhr = new XMLHttpRequest();
 
         xhr.open('POST', '{host}/client/callback/Recall/966128519f610498a7df19b1aa045b6f', true);
