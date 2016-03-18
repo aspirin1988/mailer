055d916d9877f78a4a12025500ab76dd8b1d9c2d
Sergey <aspirin_1988@mail.ru>
2016-03-18 11:50:47 +0600

    Отправка с картинками

--- app/client/models/callback.php
+++ app/client/models/callback.php
@@ -32,12 +32,32 @@ class callback extends Models
         $this->save_message($rest);
         if ($siteData) {
             $str=file_get_contents(BASE_PATH.DS.'app'.DS.'client'.DS.'views'.DS.'mail.html'); //$this->db->insert('email_massage',$rest);
-            $str=str_replace('{host}',HOST_NAME,$str);
+            //$str=str_replace('{host}',HOST_NAME,$str);
+            //$image = file_get_contents(PUBLIC_PATH.DS.'resources'.DS.'callback'.DS.'images'.DS.'mail'.DS.'001.png');
+
+            $url = explode('<img src="{host}',$str);
+            $image='';
+            unset($url[0]);
+            foreach ($url as $key =>$value)
+            {
+                $i=stripos($value,'"');
+                $src = substr($value,0,$i);
+                $j=stripos($src,'.');
+                $e=strripos($src,'/')+1;
+                $cid = substr($src,$e,$j-$e);
+                $type= substr($src,strripos($src,'.')+1,strlen($src));
+                $mime= 'image/'.$type;
+
+                $image[$key]=["url"=>PUBLIC_PATH.$src,'cid'=>$cid,'name'=>$cid.'.'.$type,'type'=>$type,'mime'=>$mime];
+                $host ='{host}'.$src;
+                $str=str_replace($host,'cid:'.$cid,$str);
+            }
+            //$str=str_replace('{001}','src="data:image/png;base64,'.base64_encode($image).'"',$str);
             foreach ($rest as $key=>$value) {
                 if ($key!='md5') $str=str_replace('{'.$key.'}',$value,$str);
             }
-            $result =$this->send_smtp_html($str,[$siteData[0]['cc_mail']], 'TEST', $siteData[0]);
-            return $result;
+            $result =$this->send_smtp_html($str,[$siteData[0]['cc_mail']], 'TEST', $siteData[0],$image);
+            return $str;
         }
         else
         {
@@ -79,7 +99,7 @@ class callback extends Models
         }
     }
 
-    function send_smtp_html($str,$email,$subject,$config){
+    function send_smtp_html($str,$email,$subject,$config,$image){
         $mail = new \library\mailer\PHPMailer();
         $to=array();
 
@@ -104,10 +124,13 @@ class callback extends Models
         $mail->Body    = $str;
         $mail->AltBody = htmlspecialchars_decode ($str);
         $mess=array();
+        foreach ($image as $key => $value) {
+            $mail->AddEmbeddedImage($value['url'], $value['cid'], $value['name'], 'base64', $value['mime']);
+        }
         foreach($email as $key=>$value){
             $mail->addAddress($value);
             if(!$mail->send()) {
-                $mess[$key]=(array('code'=>'0','text'=>'Message could not be sent.','email'=>$value, 'error'=>$mail->ErrorInfo,'email'=>$email)) ;
+                $mess[$key]=(array('code'=>'0','text'=>'Message could not be sent.','email'=>$value, 'error'=>$mail->ErrorInfo)) ;
             } else {
                 $mess[$key]=(array('code'=>'1','text'=>'Message has been sent','email'=>$value)) ;
             }
