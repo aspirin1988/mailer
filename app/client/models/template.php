19b47a83af97778aa0e9802c422cfeb1d3aa02e0
serg <aspirin_1988@mail.ru>
2016-06-11 12:40:02 +0600

    /start

--- app/client/models/template.php
+++ app/client/models/template.php
@@ -17,10 +17,46 @@ class template extends Models
     {
         $site=$this->permission($name);
         if ($site['data']) {
-            $text =$this->getOptions($site['data'][0]['id']);
-            $text=json_decode($text['data'][0]['text'],true);
+            $data =$this->getOptions($site['data'][0]['id']);
+            $text=json_decode($data['data'][0]['text'],true);
+            $widgets = json_decode($data['data'][0]['widgets'],true);
             $path = CALLBACK . DS . 'html' . DS . 'index.html';
+            print_r($widgets);
             $template = file_get_contents($path);
+
+            foreach($widgets as $key => $value)
+            {
+                foreach($value as $key1 => $value1) {
+                    if ($value1=='true') {
+                        $path = CALLBACK . DS . 'html' . DS . 'widgets' . DS . $key1 . '-popup-btn.html';
+                        $popup = file_get_contents($path);
+                        $template = str_replace('{widgets:' . $key1 . '-popup-btn}', $popup, $template);
+
+                        $path = CALLBACK . DS . 'html' . DS . 'widgets' . DS . $key1 . '-main-widgets.html';
+                        $widgets = file_get_contents($path);
+                        $template = str_replace('{widgets:' . $key1 . '-main-widgets}', $widgets, $template);
+
+                        $path = CALLBACK . DS . 'html' . DS . 'widgets' . DS . $key1 . '-main-icon.html';
+                        $icon = file_get_contents($path);
+                        $template = str_replace('{widgets:' . $key1 . '-main-icon}', $icon, $template);
+
+
+
+                    }
+                    else
+                    {
+                        $popup = '';
+                        $template = str_replace('{widgets:' . $key1 . '-popup-btn}', $popup, $template);
+                        $widgets = '';
+                        $template = str_replace('{widgets:' . $key1 . '-main-widgets}', $widgets, $template);
+                        $icon = '';
+                        $template = str_replace('{widgets:' . $key1 . '-main-icon}', $icon, $template);
+
+                    }
+                }
+
+            }
+
             foreach($text as $key => $value)
             {
                 foreach($value as $key1 => $value1)
